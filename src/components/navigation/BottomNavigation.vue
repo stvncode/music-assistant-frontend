music-assistant/frontend

feat(home): ui refresh

src/components/navigation/BottomNavigation.vue
@@ -1,0 +1,74 @@
+<template>
+  <v-bottom-navigation :height="height" bg-color="black" grow role="navigation">
+    <v-btn
+      v-for="menuItem of menuItems.filter((x) => !x.isLibraryNode)"
+      :key="menuItem.label"
+      :to="menuItem.path"
+      :aria-label="$t(menuItem.label)"
+      tabindex="0"
+      variant="text"
+      active-color="white"
+      base-color="grey"
+    >
+      <v-icon size="xx-large">{{ menuItem.icon }}</v-icon>
+      <span class="menuButton">{{ $t(menuItem.label) }}</span>
+    </v-btn>
+
+    <v-menu>
+      <template #activator="{ props }">
+        <v-btn
+          v-bind="props"
+          aria-haspopup="true"
+          :aria-expanded="isLibraryOpen.toString()"
+          aria-label="Open library menu"
+          base-color="grey"
+        >
+          <v-icon size="xx-large">mdi-bookshelf</v-icon>
+          <span class="menuButton">{{ $t("library") }}</span>
+        </v-btn>
+      </template>
+
+      <v-list>
+        <v-list-item
+          v-for="menuItem of menuItems.filter((x) => x.isLibraryNode)"
+          :key="menuItem.label"
+          :title="$t(menuItem.label)"
+          :prepend-icon="menuItem.icon"
+          :to="menuItem.path"
+        />
+      </v-list>
+    </v-menu>
+  </v-bottom-navigation>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import { getMenuItems } from "./utils/getMenuItems";
+
+export interface Props {
+  height: number;
+}
+defineProps<Props>();
+
+const menuItems = getMenuItems();
+
+const isLibraryOpen = ref(false);
+</script>
+
+<style>
+.menuButton {
+  font-weight: 350;
+  font-size: x-small;
+  font-stretch: condensed;
+  text-transform: none;
+  margin-top: 5px;
+}
+
+.v-btn--active > .v-btn__overlay {
+  background: black !important;
+}
+
+.v-slide-group-item--active {
+  opacity: 100%;
+}
+</style>

src/layouts/default/Footer.vue
@@ -1,79 +1,59 @@
 <template>
   <!-- gradient background panel to make the footer player more elevated (and hide content behind it)-->
   <div
-    v-if="useFloatingPlayer"
+    v-if="getBreakpointValue({ breakpoint: 'tablet' })"
     :class="$vuetify.theme.current.dark ? 'gradient-dark' : 'gradient-light'"
     :style="`
       position: fixed;
       width: 100%;
-      height: ${bottomNavHeight + (getBreakpointValue('bp3') ? 100 : 70)}px;
+      height: 180px;
       bottom: 0px;
       z-index: 999;
     `"
   ></div>
   <BottomNavigation
-    v-if="store.navigationMenuStyle == 'horizontal'"
     :height="bottomNavHeight"
+    v-if="getBreakpointValue({ breakpoint: 'tablet' })"
   />
   <v-footer
-    bottom
-    fixed
-    :class="`d-flex flex-column ${
-      useFloatingPlayer
+    app
+    color="black"
+    :class="`py-0 px-0 ${
+      getBreakpointValue({ breakpoint: 'tablet' })
         ? 'mediacontrols-player-float'
         : 'mediacontrols-player-default'
     }`"
-    :style="`bottom: ${footerMarginBottom}px;`"
-    elevation="10"
-    app
   >
-    <Player :use-floating-player="useFloatingPlayer" />
+    <Player
+      :use-floating-player="getBreakpointValue({ breakpoint: 'tablet' })"
+    />
   </v-footer>
 </template>
 
 <script setup lang="ts">
-import BottomNavigation from "./BottomNavigation.vue";
+import { getBreakpointValue } from "@/plugins/breakpoint";
 import Player from "./PlayerOSD/Player.vue";
 import { store } from "@/plugins/store";
 import { computed } from "vue";
-import { getBreakpointValue } from "@/plugins/breakpoint";
-
-const useFloatingPlayer = computed(() => {
-  return store.navigationMenuStyle == "horizontal";
-});
+import BottomNavigation from "@/components/navigation/BottomNavigation.vue";
 
 const bottomNavHeight = computed(() => {
-  if (store.navigationMenuStyle != "horizontal") return 0;
   if (store.isInStandaloneMode) {
     // for iOS standalone we need extra padding at the bottom due to the apphandle
     return 100;
   }
   return 80;
 });
-
-const footerMarginBottom = computed(() => {
-  if (useFloatingPlayer.value) return bottomNavHeight.value + 5;
-  return 0;
-});
 </script>
 
 <style>
 .mediacontrols-player-float {
-  width: 100%;
-  padding: 0px !important;
-  left: 5px !important;
-  width: calc((100% - 10px) - 0px) !important;
+  display: flex;
+  flex-direction: column;
+  margin: 10px;
+  margin-bottom: 0;
+  width: calc((100% - 20px) - 0px) !important;
   border-radius: 10px !important;
-  border-color: rgba(100, 100, 100, 0.9) 75%;
-  border-style: solid;
-  border-width: thin;
-}
-
-.mediacontrols-player-default {
-  width: 100%;
-  border-top-style: ridge;
-  padding: 0px;
-  border-top: 1px solid #20202035;
 }
 
 .gradient-dark {

stvncode/music-assistant-frontend

src/components/navigation/BottomNavigation.vue
@@ -1,0 +1,79 @@
+<template>
+  <v-bottom-navigation
+    :height="height"
+    bg-color="default"
+    grow
+    role="navigation"
+  >
+    <v-btn
+      v-for="menuItem of menuItems.filter((x) => !x.isLibraryNode)"
+      :key="menuItem.label"
+      :to="menuItem.path"
+      :aria-label="$t(menuItem.label)"
+      tabindex="0"
+      variant="text"
+      active-color="fg"
+      base-color="grey"
+    >
+      <v-icon size="xx-large">{{ menuItem.icon }}</v-icon>
+      <span class="menuButton">{{ $t(menuItem.label) }}</span>
+    </v-btn>
+
+    <v-menu>
+      <template #activator="{ props }">
+        <v-btn
+          v-bind="props"
+          aria-haspopup="true"
+          :aria-expanded="isLibraryOpen.toString()"
+          aria-label="Open library menu"
+          base-color="grey"
+        >
+          <v-icon size="xx-large">mdi-bookshelf</v-icon>
+          <span class="menuButton">{{ $t("library") }}</span>
+        </v-btn>
+      </template>
+
+      <v-list>
+        <v-list-item
+          v-for="menuItem of menuItems.filter((x) => x.isLibraryNode)"
+          :key="menuItem.label"
+          :title="$t(menuItem.label)"
+          :prepend-icon="menuItem.icon"
+          :to="menuItem.path"
+        />
+      </v-list>
+    </v-menu>
+  </v-bottom-navigation>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import { getMenuItems } from "./utils/getMenuItems";
+
+export interface Props {
+  height: number;
+}
+defineProps<Props>();
+
+const menuItems = getMenuItems();
+
+const isLibraryOpen = ref(false);
+</script>
+
+<style>
+.menuButton {
+  font-weight: 350;
+  font-size: x-small;
+  font-stretch: condensed;
+  text-transform: none;
+  margin-top: 5px;
+}
+
+.v-btn--active > .v-btn__overlay {
+  background: rgb(var(--v-theme-default)) !important;
+}
+
+.v-slide-group-item--active {
+  opacity: 100%;
+}
+</style>

src/layouts/default/Footer.vue
@@ -1,79 +1,59 @@
 <template>
   <!-- gradient background panel to make the footer player more elevated (and hide content behind it)-->
   <div
-    v-if="useFloatingPlayer"
+    v-if="getBreakpointValue({ breakpoint: 'tablet' })"
     :class="$vuetify.theme.current.dark ? 'gradient-dark' : 'gradient-light'"
     :style="`
       position: fixed;
       width: 100%;
-      height: ${bottomNavHeight + (getBreakpointValue('bp3') ? 100 : 70)}px;
+      height: 180px;
       bottom: 0px;
       z-index: 999;
     `"
   ></div>
   <BottomNavigation
-    v-if="store.navigationMenuStyle == 'horizontal'"
     :height="bottomNavHeight"
+    v-if="getBreakpointValue({ breakpoint: 'tablet' })"
   />
   <v-footer
-    bottom
-    fixed
-    :class="`d-flex flex-column ${
-      useFloatingPlayer
+    app
+    color="default"
+    :class="`py-0 px-0 ${
+      getBreakpointValue({ breakpoint: 'tablet' })
         ? 'mediacontrols-player-float'
         : 'mediacontrols-player-default'
     }`"
-    :style="`bottom: ${footerMarginBottom}px;`"
-    elevation="10"
-    app
   >
-    <Player :use-floating-player="useFloatingPlayer" />
+    <Player
+      :use-floating-player="getBreakpointValue({ breakpoint: 'tablet' })"
+    />
   </v-footer>
 </template>
 
 <script setup lang="ts">
-import BottomNavigation from "./BottomNavigation.vue";
+import { getBreakpointValue } from "@/plugins/breakpoint";
 import Player from "./PlayerOSD/Player.vue";
 import { store } from "@/plugins/store";
 import { computed } from "vue";
-import { getBreakpointValue } from "@/plugins/breakpoint";
-
-const useFloatingPlayer = computed(() => {
-  return store.navigationMenuStyle == "horizontal";
-});
+import BottomNavigation from "@/components/navigation/BottomNavigation.vue";
 
 const bottomNavHeight = computed(() => {
-  if (store.navigationMenuStyle != "horizontal") return 0;
   if (store.isInStandaloneMode) {
     // for iOS standalone we need extra padding at the bottom due to the apphandle
     return 100;
   }
   return 80;
 });
-
-const footerMarginBottom = computed(() => {
-  if (useFloatingPlayer.value) return bottomNavHeight.value + 5;
-  return 0;
-});
 </script>
 
 <style>
 .mediacontrols-player-float {
-  width: 100%;
-  padding: 0px !important;
-  left: 5px !important;
-  width: calc((100% - 10px) - 0px) !important;
+  display: flex;
+  flex-direction: column;
+  margin: 10px;
+  margin-bottom: 0;
+  width: calc((100% - 20px) - 0px) !important;
   border-radius: 10px !important;
-  border-color: rgba(100, 100, 100, 0.9) 75%;
-  border-style: solid;
-  border-width: thin;
-}
-
-.mediacontrols-player-default {
-  width: 100%;
-  border-top-style: ridge;
-  padding: 0px;
-  border-top: 1px solid #20202035;
 }
 
 .gradient-dark {
@@ -91,5 +71,9 @@ const footerMarginBottom = computed(() => {
     rgba(255, 255, 255, 0.9) 75%,
     rgba(255, 255, 255, 0) 100%
   );
+}
+
+.v-bottom-navigation--active {
+  box-shadow: none;
 }
 </style>